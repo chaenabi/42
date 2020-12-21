924180f4666bb02770128eb7458f5cf547ba9d06
Myoung cheol Lee <mylee@c1r1s1.42seoul.kr>
2020-12-21 19:05:59 +0900

    init libft

---
+++ cadet_42/libft/completed/ft_atoi.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mylee <mylee@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/21 17:14:03 by mylee             #+#    #+#             */
+/*   Updated: 2020/12/21 17:59:29 by mylee            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		ft_isspace(char c)
+{
+	return ((c == 32) || (c >= 9 && c <= 13));
+}
+
+int		ft_atoi(const char *nptr)
+{
+	int	flag;
+	int	result;
+	int	i;
+
+	i = 0;
+	flag = 0;
+	while (nptr[i] && ft_isspace(nptr[i]))
+		i++;
+	if (nptr[i] == '-')
+		flag = 1;
+	while (nptr[i] == '-' || nptr[i] == '+')
+		i++;
+	result = 0;
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		result = 10 * result + (nptr[i++] - '0');
+	}
+	if (flag)
+		result = -result;
+	return (result);
+}
