6eb6af6d5dd15076860303ff180e687c249957bf
chaenabi <find1086@gmail.com>
2020-12-26 22:52:22 +0900

    impl bonus

--- cardet_42/libft/libft.h
+++ cardet_42/libft/libft.h
@@ -14,6 +14,7 @@
 # define LIBFT_H
 
 # include <stdlib.h>
+# include <unistd.h>
 
 typedef struct		s_list
 {
@@ -62,5 +63,16 @@ void				ft_putendl_fd(char *s, int fd);
 void				ft_putnbr_fd(int n, int fd);
 void				ft_putchar(char c);
 void				ft_putstr(char *str);
+t_list				*ft_lstnew(void *content);
+void				ft_lstadd_front(t_list **lst, t_list *new);
+int					ft_lstsize(t_list *lst);
+void				ft_lstadd_back(t_list **lst, t_list *new);
+void				ft_lstdelone(t_list *lst,
+								void (*del)(void *));
+void				ft_lstclear(t_list **lst,
+								void (*del)(void *));
+void				ft_lstiter(t_list *lst, void (*f)(void *));
+t_list				*ft_lstmap(t_list *lst, void *(*f)(void *),
+								void (*del)(void *));
 
 #endif
