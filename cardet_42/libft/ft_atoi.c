e6b4c4014dadcfaf532442ff2e25da5a9bd7d56f
chaenabi <find1086@gmail.com>
2020-12-28 10:47:55 +0900

    TARGET to NAME

--- cardet_42/libft/ft_atoi.c
+++ cardet_42/libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: mylee <mylee@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/21 17:14:03 by mylee             #+#    #+#             */
-/*   Updated: 2020/12/27 00:50:03 by mylee            ###   ########.fr       */
+/*   Updated: 2020/12/28 10:46:17 by mylee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ int				ft_atoi(const char *nptr)
 	while (nptr[i] == '-' || nptr[i] == '+')
 		i++;
 	result = 0;
-	while (nptr[i] >= '0' && nptr[i] <= '9')
+	while (nptr[i] && nptr[i] >= '0' && nptr[i] <= '9')
 	{
 		result = 10 * result + (nptr[i++] - '0');
 	}
