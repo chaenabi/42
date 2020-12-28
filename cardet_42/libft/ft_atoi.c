d23f8084cebb38e228ac64b28af536dad2515ba2
Myoung cheol Lee <mylee@c1r2s2.42seoul.kr>
2020-12-28 11:22:36 +0900

    fix ft_atoi, rename bonus

--- cardet_42/libft/ft_atoi.c
+++ cardet_42/libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: mylee <mylee@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/21 17:14:03 by mylee             #+#    #+#             */
-/*   Updated: 2020/12/28 10:46:17 by mylee            ###   ########.fr       */
+/*   Updated: 2020/12/28 11:08:16 by mylee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,19 +24,24 @@ int				ft_atoi(const char *nptr)
 	int			i;
 
 	i = 0;
-	flag = 0;
+	flag = 1;
 	while (nptr[i] && ft_isspace(nptr[i]))
 		i++;
 	if (nptr[i] == '-')
-		flag = 1;
+		flag = -1;
 	while (nptr[i] == '-' || nptr[i] == '+')
 		i++;
 	result = 0;
 	while (nptr[i] && nptr[i] >= '0' && nptr[i] <= '9')
 	{
-		result = 10 * result + (nptr[i++] - '0');
+		if (result * flag > 2147483647)
+			return (-1);
+		else if (result * flag < -2147483648)
+			return (0);
+		else
+			result = 10 * result + (nptr[i++] - '0');
 	}
-	if (flag)
+	if (flag == -1)
 		result = -result;
 	return (result);
 }
