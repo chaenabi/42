6ba77f382159cc83ec2581f8d9b69e5ccf6fc909
chaenabi <find1086@gmail.com>
2020-12-30 22:51:01 +0900

    fix ft_strmapi.c, ft_substr.c

--- cardet_42/libft/ft_strmapi.c
+++ cardet_42/libft/ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: mylee <mylee@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/26 23:33:05 by mylee             #+#    #+#             */
-/*   Updated: 2020/12/28 10:29:32 by mylee            ###   ########.fr       */
+/*   Updated: 2020/12/30 22:46:47 by mylee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,16 @@
 
 char		*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	size_t	s_len;
-	size_t	i;
 	char	*result;
+	size_t	i;
 
+	if (!s || !f || !(result = ft_strdup(s)))
+		return (NULL);
 	i = 0;
-	if (*s == '\0')
-		return (NULL);
-	s_len = ft_strlen(s);
-	if (!(result = (char *)malloc(sizeof(char) * (s_len + 1))))
-		return (NULL);
-	while (i < s_len)
+	while (result[i])
 	{
-		result[i] = f(i, s[i]);
+		result[i] = f(i, result[i]);
 		i++;
 	}
-	result[i] = '\0';
 	return (result);
 }
--- cardet_42/libft/ft_substr.c
+++ cardet_42/libft/ft_substr.c
@@ -6,27 +6,20 @@
 /*   By: mylee <mylee@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/26 23:31:45 by mylee             #+#    #+#             */
-/*   Updated: 2020/12/28 10:33:06 by mylee            ###   ########.fr       */
+/*   Updated: 2020/12/30 22:48:39 by mylee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char		*ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char	*str;
-	size_t	size;
+	char	sub_str[len + 1];
 
-	if (*s == '\0')
+	if (!s)
 		return (NULL);
-	if (!(str = (char *)malloc(sizeof(char) * (len + 1))))
-		return (NULL);
-	size = ft_strlen(s);
-	if (size <= start)
-	{
-		str[0] = '\0';
-		return (str);
-	}
-	ft_strlcpy(str, s + start, len + 1);
-	return (str);
+	if (ft_strlen(s) <= start)
+		return (ft_strdup(""));
+	ft_strlcpy(sub_str, s + start, len + 1);
+	return (ft_strdup(sub_str));
 }
